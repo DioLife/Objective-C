0d08b140264debb17fae62ea420c820a11553501
hello <hello@hellodeMacBook-Pro-2.local>
2018-12-16 18:45:05 +0800

    App跳转
    从swift App跳到这个App,这边可以接收到传来的值

--- ObjectiveC/AppDelegate.m
+++ ObjectiveC/AppDelegate.m
@@ -69,6 +69,13 @@
     self.window.rootViewController = tabBarCtrl;
 }
 
+-(BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    // 此处url的值就是APP_1 openURL的url
+    NSString *paraStr = url.absoluteString;
+    NSLog(@"%@", paraStr);
+    return YES;
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
