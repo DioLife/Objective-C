25067d66c3a18b5e85a940419a13ec4b1b34c32c
hello <hello@hellodeMacBook-Pro-2.local>
2019-03-19 19:31:25 +0800

    add

---
+++ ObjectiveC/language/Audio/AVPlayerViewController.m
@@ -0,0 +1,32 @@
+//
+//  AVPlayerViewController.m
+//  ObjectiveC
+//
+//  Created by hello on 2019/3/19.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+#import "AVPlayerViewController.h"
+
+@interface AVPlayerViewController ()
+
+@end
+
+@implementation AVPlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
